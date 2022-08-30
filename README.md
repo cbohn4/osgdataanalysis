be6676f2d9408dc08a9603f42927d61810958491
cbohn <cbohn4@unl.edu>
2022-08-30 10:58:08 -0500

    Add XDMoD Integration

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
 # OSGDataAnalysis
-Notebook for finding OSG Eligible Jobs using sacct results.
+Python script for finding OSG/PATh Eligible Jobs using sacct results.
 
 ## Defitionions: 
-There are two catergories of jobs being looked at, OSG Ideal Jobs and OSG Limit Jobs. 
+There are two catergories of jobs being looked at, jobs that would be good candidates for the Open Science Grid (OSG), and the Partnership to Advanced Throughput Computing (PATh).
 
-OSG Ideal jobs are ones that would be ideal canidiates to run on the open science grid due to their small resource and times requests.
-Similarly, OSG Limit jobs are ones that would still work well on the Open Science Grid, but would be harder to finish or find available resources.
+Both categories would be requeired to be single node jobs and a limit of 1 GPU. 
+OSG eligible jobs would need to meet the job resource requirements below:
+* <= 8 CPU Cores
+* <= 8GB of Memory
+* <= 24 hours of runtime
 
-Both types of jobs are single node and single core, the differences are how long the job runs for, and how much memory is requested. 
+PATh expands on these requirements:
+* <= 64 CPU Cores
+* <= 200GB of Memory
+* <= 40 hours of runtime
 
-OSG Ideal has 4GB of memory requested or less and 10 hours in length or less, while OSG Limit is 4-10GB of memory requested and a day or less in runtime.
+## Data Generation:
+
+### sacct
+The primary method for generating data is using Slurm's `sacct` utility. The example command below would pull the required data for a week of jobs on the current cluster. Additional data can be appended to the output file, but would need the `-n` flag added to the sacct command to remove the header from the output. 
+`sacct -P -a -S 2022-08-20 -E 2022-08-27 -o JobID,Cluster,User,Group,ReqCPUS,ReqNodes,ReqMem,ReqTRES,TimeLimitRaw > ~/sacct.csv`
+Adding more data:
+`sacct -n -P -a -S 2022-08-13 -E 2022-08-20 -o JobID,Cluster,User,Group,ReqCPUS,ReqNodes,ReqMem,ReqTRES,TimeLimitRaw >> ~/sacct.csv`
+
+**IMPORTANT NOTE:** Do not `sacct` run more than a few weeks at a time depending on the size of the cluster and resources available to the slurmdb database. Pulling too much data from `sacct` can cause the slurm process to crash and prevent slurm from processing jobs cluster wide. 
+
+### XDMoD
+If you have an XDMoD instance, version 9.0 or greater, larger data requests are better served by the XDMoD `mod_hpcdb` database. By default, this will include all HPC resources within XDMoD. 
+`SELECT local_job_id_raw,resource_id,username,groupname,cpu_req,nodecount,mem_req,gpucount,timelimit FROM hpcdb_jobs WHERE submit_time >= 1660953600 AND submit_time <= 1661558400 INTO OUTFILE '/tmp/xdmod.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';`
 
 
 
-## In the output file, there are six columns:
-user: Username of the user
-
-osgIdeal: Number of jobs meeting the OSG Ideal Requirements
-
-osgLimit: Number of jobs meeting the OSG Limit Requirements
-
-total: Total Number of jobs ran by the user. 
-
-percIdeal: Decimal percentage of jobs meeting the OSG Ideal Requirements
-
-percLimit: Decimal percentage of jobs meeting the OSG Limit Requirements
+## In the output files, there are four columns:
+username/groupname: Group or username
+osg: Number of OSG eligible jobs for the group/user.
+path: Number of PATh eligible jobs for the group/user.
+total: Number of jobs analyzed for the group/user. 
--- osg_analysis.py
+++ osg_analysis.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import numpy as np
 import sys
+from optparse import OptionParser
+
+
+parser = OptionParser()
+parser.add_option("-i","--input", action="store", dest="input_file",
+        help="(required) Input file containing sacct or XDMoD data.")
+parser.add_option("-x","--xdmod", action="store_true", dest="xdmod",
+        help="Use XDMoD data instead of sacct data.")
+parser.usage = "%prog [options] LOGIN"
+(options, args) = parser.parse_args()
 
 
 #  sacct -P -a -S 2022-08-20 -E 2022-08-27 -o JobID,Cluster,User,Group,ReqCPUS,ReqNodes,ReqMem,ReqTRES,TimeLimitRaw > ~/test.sacct
@@ -21,18 +31,25 @@ def mem_parse(cores,nodes,mem):
     if "n" == mem[-1]:
         return float(mem[:-2]) * nodes * 1024**(suffixes.index(mem[-2])-2)
 
-input_file = sys.argv[1]
-
-df = pd.read_csv(input_file,delimiter="|")
+if options.xdmod:
+    df = pd.read_csv(options.input_file,delimiter=",",names=["JobID","Cluster","User","Group","ReqCPUS","ReqNodes","ReqMem","ReqGPU","TimelimitRaw"])
+else:
+    df = pd.read_csv(options.input_file,delimiter="|")
 # Get rid of slurm steps. 
 df = df.dropna()
+
 # Clean up in the event of duplicate jobs from combining multiple sacct runs
 df = df.drop_duplicates(subset=["JobID", "Cluster"], keep="first")
+
 # Get GPU Requests
-df = df.assign(ReqGPU=gpu_count(df["ReqTRES"]))
+if not options.xdmod:
+    df = df.assign(ReqGPU=gpu_count(df["ReqTRES"]))
+
 user_list = np.unique(list(df["User"]))
 group_list = np.unique(list(df["Group"]))
 
+
+# Setup data structure
 total_statistics = {"osg":0,"path":0,"total":len(df)}
 
 user_job_count = {}
@@ -44,13 +61,14 @@ for group in group_list:
     group_job_count[group] = {"osg":0, "path":0, "total":len(df[df['Group'] == group])}
 
 
-# Current Eligibility: <= 64 Cores, <= 1 GPU, 1 Node, <=200GB Memory <= 40 hours, <= 1 GPU
+# Get data organized
 
+# Current Eligibility: <= 64 Cores, <= 1 GPU, 1 Node, <=200GB Memory <= 40 hours, <= 1 GPU
 for r, job in df.iterrows():
     mem_req = mem_parse(job["ReqCPUS"],job["ReqNodes"],job["ReqMem"])
     if job["ReqNodes"] == 1 and job["TimelimitRaw"] <= 40 * 3600: # Basic filter
 
-        if job["ReqGPU"] == 0 and job["ReqCPUS"] <= 8 and mem_req <= 8 and job["TimelimitRaw"] <= 24 * 3600: # OSG Requirements
+        if job["ReqCPUS"] <= 8 and mem_req <= 8 and job["TimelimitRaw"] <= 24 * 3600: # OSG Requirements
             user_job_count[job["User"]]["osg"] += 1
             group_job_count[job["Group"]]["osg"] += 1
             total_statistics["osg"] += 1
@@ -59,8 +77,31 @@ for r, job in df.iterrows():
             user_job_count[job["User"]]["path"] += 1
             group_job_count[job["Group"]]["path"] += 1
             total_statistics["path"] += 1
-    if job["User"] == "osg":
-        print("NOW",user_job_count["osg"])
+
+# Organize for CSV output
+user_output = {"username":[],"osg":[],"path":[],"total":[]}
+group_output = {"groupname":[],"osg":[],"path":[],"total":[]}
+
+for user in user_job_count.keys():
+    user_output["username"].append(user)
+    user_output["osg"].append(user_job_count[user]["osg"])
+    user_output["path"].append(user_job_count[user]["path"])
+    user_output["total"].append(user_job_count[user]["total"])
+
+for group in group_job_count.keys():
+    group_output["groupname"].append(group)
+    group_output["osg"].append(group_job_count[group]["osg"])
+    group_output["path"].append(group_job_count[group]["path"])
+    group_output["total"].append(group_job_count[group]["total"])
+
+pd.DataFrame(user_output).to_csv(".".join(options.input_file.split(".")[:-1]) + "_users.csv", index=False)
+pd.DataFrame(group_output).to_csv(".".join(options.input_file.split(".")[:-1])+ "_groups.csv", index=False)
+
+print("User output file can be found at: " + ".".join(options.input_file.split(".")[:-1]) + "_users.csv")
+print("Group output file can be found at: " + ".".join(options.input_file.split(".")[:-1])+ "_groups.csv")
+
+print(f"{np.round((total_statistics['osg']*100)/(total_statistics['total']),2)} % of {(total_statistics['total'])} jobs were eligible for the OSG.")
+print(f"{np.round((total_statistics['path']*100)/(total_statistics['total']),2)} % of {(total_statistics['total'])} jobs were eligible for PATh.")
 
 
 
